e0b26ed014a37a3ea6a44671fc83cc26f93dc296
jotty <bard143games@gmail.com>
2021-08-19 10:54:33 +0300

    Added IntentHandler and IntentHandlerCollection classes

---
+++ bot/intent_handler.py
@@ -0,0 +1,18 @@
+import dataclasses
+from typing import Coroutine, List
+
+
+@dataclasses.dataclass
+class IntentHandler:
+    name: str
+    priority: int
+    handler: Coroutine
+
+
+class IntentHandlersCollection(List):
+    def sort_by_priority(self):
+        self.sort(key=lambda handler: handler.priority)
+
+    def append(self, obj) -> None:
+        super().append(obj)
+        self.sort_by_priority()
