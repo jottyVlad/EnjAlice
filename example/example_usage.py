d1de85efc6ede8fddfc7bf6bbe4316f12a4c3219
jotty <bard143games@gmail.com>
2021-08-19 10:55:27 +0300

    Updated poetry; crated example of usage bot

---
+++ example/example_usage.py
@@ -0,0 +1,26 @@
+from bot.bot import Bot
+from bot.dispatcher import Dispatcher
+from bot.request import AliceRequest
+from bot.response import AliceResponse
+
+
+dp = Dispatcher()
+bot = Bot(dp=dp)
+
+bot.set_webhook_host("127.0.0.1")
+bot.set_webhook_port(8888)
+bot.set_webhook_path("/alice")
+
+dp.set_start_text("Привет, я тестовый навык для разработки оболочки над моим API enj-alice")
+
+
+@dp.message_handler(priority=1000, intent="YANDEX.HELP")
+async def handle_help(request: AliceRequest,
+                      response: AliceResponse):
+    # Here we can process data in request
+    # Then fill data in response
+    response.response.text = "У меня пока что нет команд, но они обязательно будут!"
+    # And return AliceResponse object
+    return response
+
+bot.run()
