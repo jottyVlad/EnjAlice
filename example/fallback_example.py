fc8c6e5caaff03892516c10eb24daab5944fe075
Crystal Melting Dot <stresspassing@gmail.com>
2021-08-21 17:17:18 +0300

    fix examples

--- example/fallback_example.py
+++ example/fallback_example.py
@@ -2,19 +2,18 @@ from enjalice import Dispatcher, Bot, AliceRequest
 from enjalice.response import text
 
 
-dp = Dispatcher()
+async def start_handler(_: AliceRequest):
+    return text(
+        msg="Привет, я тестовый навык для разработки оболочки над моим API enj-alice"
+    )
+
+
+dp = Dispatcher(start_handler)
 bot = Bot(dp=dp)
 
 bot.webhook_host = "127.0.0.1"
 bot.webhook_port = 8888
 bot.webhook_path = "/alice"
-
-
-@dp.start_handler()
-async def start_handler(_: AliceRequest):
-    return text(
-        msg="Привет, я тестовый навык для разработки оболочки над моим API enj-alice"
-    )
 
 
 @dp.message_handler(priority=100)
