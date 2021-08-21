63d410a8b4205053367770ac805f783e068a7668
Crystal Melting Dot <stresspassing@gmail.com>
2021-08-21 16:38:22 +0300

    more accurate hint for message handler

--- enjalice/_hints.py
+++ enjalice/_hints.py
@@ -1,13 +1,14 @@
 from typing import Callable, Awaitable, Optional, Union
 
 from .response import AliceResponse
+from .request import AliceRequest
 
 
 HandlerResult = Optional[AliceResponse]
 
 
 MessageHandlerFunction = Callable[
-    ...,
+    [AliceRequest],
     Union[
         HandlerResult,
         Awaitable[HandlerResult]
